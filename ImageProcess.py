4e23538b5b9fb73bba084c281fa61d1dbb714dbd
Yang Lu <xinyanglu664@gmail.com>
2024-10-04 16:39:25 -0400

    WIP: implement initial preprocessing for image to text recognition

--- ImageProcess.py
+++ ImageProcess.py
@@ -21,12 +21,49 @@ def getImageFromClipboard():
         return None
 
 def findBoard(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
-    # blur = cv2.GaussianBlur(gray,(5,5),0)
-    th2 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv2.THRESH_BINARY,13,4)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.resize(gray, (250,250))
+    
+    # blur = cv2.GaussianBlur(gray,(3,3),0)
+    thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+            cv2.THRESH_BINARY,33,10)
+    
+    # rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 10))
+    # threshed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, rect_kernel)
 
-    cv2.imshow("Clipboard Image", th2)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    lowerBound = 5 * 10
+    upperBound = 30 * 30
+
+    letters = []
+    for contour in contours:
+        # Get bounding box around contour
+        area = cv2.contourArea(contour)
+        if area < lowerBound or area > upperBound:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+
+        # if(gray[y][x] != 255): #top left corner of contour isn't white
+        #     continue
+
+        # Crop the region containing the letter
+        letter_image = thresh[y:y+h, x:x+w]
+
+        # Apply OCR to the cropped letter
+        text = pytesseract.image_to_string(letter_image, config='-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 7 preserve_interword_spaces=1')  # '--psm 10' treats it as a single character
+        if text.strip() == '' or len(text.strip()) > 1:
+            letter_image = thresh[y-5:y+h+5, x-5:x+w+5]
+            text = pytesseract.image_to_string(letter_image, config='-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 7 preserve_interword_spaces=1')  # '--psm 10' treats it as a single character
+
+        letters.append(text.strip())
+
+        # Optional: Draw bounding box on original image for debugging
+        cv2.rectangle(gray, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    print(letters)
+
+    cv2.imshow("Clipboard Image", gray)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
